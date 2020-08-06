85c8661254047fa52152fb1a795d037297254ffa
sahithimuthyala03 <69254657+sahithimuthyala03@users.noreply.github.com>
2020-08-06 15:26:59 -0400

    Add files via upload

---
+++ MiniProject2/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Sahithi Muthyala on 8/6/20.
+//  Copyright © 2020 Sahithi Muthyala. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+class SecondViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        
+    }
+}
+
+class ThirdViewController: UIViewController{
+    override func viewDidLoad(){
+        
+    }
+}
+
+class FourthViewController: UIViewController{
+    override func viewDidLoad(){
+        
+    }
+}
